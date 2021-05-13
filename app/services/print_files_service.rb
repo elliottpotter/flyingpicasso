120e5444051296e6baf0155c9ed353676b097243
Elliott Potter <elliott@shipt.com>
2021-05-12 19:39:47 -0500

    add background removal and ivnersion

--- app/services/print_files_service.rb
+++ app/services/print_files_service.rb
@@ -30,11 +30,13 @@ class PrintFilesService
 
     until filtered_rows.empty?
       # Get the SKU of incoming items, and determine how many to print per sheet
-      item_sku       = filtered_rows[0][:item_sku]
-      item_map       = SHEET_ITEM_MAP[item_sku]
-      these_n_images = filtered_rows.shift(item_map[:qty_per_sheet])
-      order_ids      = these_n_images.map {|row| row[:order_id] }.uniq.sort.join(', ')
-      final_filename = these_n_images.map {|row| "#{row[:order_id]}-(#{these_n_images.select{|r| r[:order_id] == row[:order_id] }.count})"}.uniq.sort.join('_')
+      item_sku                = filtered_rows[0][:item_sku]
+      item_map                = SHEET_ITEM_MAP[item_sku]
+      these_n_images          = filtered_rows.shift(item_map[:qty_per_sheet])
+      order_ids               = these_n_images.map {|row| row[:order_id] }.uniq.sort.join(', ')
+      original_image_urls     = these_n_images.map {|row| row[:image_url] }.uniq.sort.join(', ')
+      final_filename          = these_n_images.map {|row| "#{row[:order_id]}-(#{these_n_images.select{|r| r[:order_id] == row[:order_id] }.count})"}.uniq.sort.join('_')
+      raw_with_inversions_url = nil 
 
       transformations = these_n_images.each_with_index.collect do |image_data, index|
         base_64_image_url = Base64.encode64(image_data[:image_url]).gsub("\n", '')
@@ -45,7 +47,8 @@ class PrintFilesService
           width: (item_map[:image_width] * BASE_MULTIPLE).to_i,
           x: (position_map[(index + 1).to_s][:x] * BASE_MULTIPLE).to_i,
           y: (position_map[(index + 1).to_s][:y] * BASE_MULTIPLE).to_i,
-          gravity: 'north_west'
+          gravity: 'north_west',
+          background_removal: item_sku == 'CUS-PVC-BLK' ? 'cloudinary_ai' : ''
         }
       end
 
@@ -72,7 +75,27 @@ class PrintFilesService
           y: (position_map[(index + 1).to_s][:y] * BASE_MULTIPLE).to_i,
           gravity: 'north_west'
         }]
-      end      
+      end
+      
+      if item_sku == 'CUS-PVC-BLK' 
+        with_inversion_transformations = these_n_images.each_with_index.collect do |image_data, index|
+          base_64_image_url = Base64.encode64(image_data[:image_url]).gsub("\n", '')
+          position_map = send("#{item_map[:item_name]}_position_map".to_sym)
+
+          {
+            overlay: "fetch:#{base_64_image_url}",
+            effect: 'colorize:100',
+            color: '#000000',
+            background_removal: 'cloudinary_ai',
+            width: (item_map[:image_width] * BASE_MULTIPLE).to_i,
+            x: (position_map[(index + 1).to_s][:x] * BASE_MULTIPLE).to_i,
+            y: (position_map[(index + 1).to_s][:y] * BASE_MULTIPLE).to_i,
+            gravity: 'north_west'
+          }
+        end
+
+        raw_with_inversions_url = Cloudinary::Utils.cloudinary_url(BASE_IMAGE, transformation: with_inversion_transformations.flatten)
+      end
 
       raw_url = Cloudinary::Utils.cloudinary_url(BASE_IMAGE, transformation: transformations)
       raw_with_order_ids_url = Cloudinary::Utils.cloudinary_url(BASE_IMAGE, transformation: with_order_id_transformations.flatten)
@@ -82,7 +105,9 @@ class PrintFilesService
         url: raw_url,
         order_ids: order_ids,
         item_sku: item_sku,
-        raw_with_order_ids_url: raw_with_order_ids_url
+        raw_with_order_ids_url: raw_with_order_ids_url,
+        raw_with_inversions_url: raw_with_inversions_url,
+        original_image_urls: original_image_urls
       }
     end
 
@@ -91,12 +116,15 @@ class PrintFilesService
       threads << Thread.new do
         image = Cloudinary::Uploader.upload(data[:url], folder: "printfiles", public_id: data[:filename], attachment: true, timeout: 180)
         image_with_order_ids = Cloudinary::Uploader.upload(data[:raw_with_order_ids_url], folder: "printfiles", public_id: "#{data[:filename]}_with_ids_#{rand(0..1000)}", attachment: true, timeout: 180)
-        
+        image_with_inversions = Cloudinary::Uploader.upload(data[:raw_with_inversions_url], folder: "printfiles", public_id: "#{data[:filename]}_with_inversions_#{rand(0..1000)}", attachment: true, timeout: 180)
+
         webhook_data = {
           image_url: image['url'],
           image_with_order_ids_url: image_with_order_ids['url'],
           order_ids: data[:order_ids],
-          item_sku: data[:item_sku]
+          item_sku: data[:item_sku],
+          original_image_urls: data[:original_image_urls],
+          raw_with_inversions_url: image_with_inversions['url']
         }
 
         headers  = { "Content-Type": "application/json" }
